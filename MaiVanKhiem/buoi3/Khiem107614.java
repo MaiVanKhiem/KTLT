c327586261f8156d67ca8d4f8e4c9a9e76527d62
MaiVanKhiem <khiemmai47@gmail.com>
2024-10-30 00:24:21 +0700

    Add files via upload

---
+++ MaiVanKhiem/buoi3/Khiem107614.java
@@ -0,0 +1,23 @@
+    package buoi3;
+    public class Khiem107614 { 
+    public static String gioiTinh(boolean gt){
+         return gt? "nam":"nữ";
+}
+public static void main(String[] args) {
+    String maSV = "107614", hoVaTen = "Mai Văn Khiêm", ngaySinh = "30/04/2006",
+    lop = "IT24A", khoa ="Công Nghệ Thông Tin", diaChi = "Đà Nẵng"; 
+    int tuoi = 22;
+    boolean gt = false;
+    System.out.println("----------TTHÔNG TIN SINH VIÊN----------");
+    System.out.println("Mã Sinh Viên: "+maSV);
+    System.out.println("Họ Và Tên: "+hoVaTen);
+    System.out.println("Ngày Sinh: "+ngaySinh);
+    System.out.println("Tuổi:"+tuoi);
+    System.out.println("Giới Tính: "+gioiTinh(gt));
+    System.out.println("Lớp: "+lop);
+    System.out.println("Khóa: "+khoa);
+    System.out.println("Địa Chỉ: "+diaChi);
+    System.out.println("-------HếT-------");
+
+    }
+}
